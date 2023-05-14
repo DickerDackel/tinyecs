55a15874ec86db2d530b261c8331fc042bfec997
Michael Lamertz <michael.lamertz@gmail.com>
2023-05-14 21:00:26 +0200

    Housekeeping

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='tinyecs',
-    version='0.0.1',
+    version='0.1.1',
     description='The teeniest, tiniest ECS system',
     author='Michael Lamertz',
+    author_email='michael.lamertz@gmail.com',
     url='https://github.com/dickerdackel/tinyecs',
-    packages=['tinyecs'],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Games/Entertainment :: Arcade',
-    ]
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
 )
