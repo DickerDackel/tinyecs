d8aa4e8ac0ff63bc001ef55a4f7d173740815be9
Michael Lamertz <michael.lamertz@gmail.com>
2023-05-14 20:54:29 +0200

    Complete rewrite

--- example/example.py
+++ example/example.py
@@ -1,61 +1,107 @@
 import tinyecs as ecs
 
-
+from enum import Enum, auto
 from dataclasses import dataclass
+from types import SimpleNamespace
 
-# Let's see, if the object will be destroyed if it's removed from the
-# registry at the end
-def destructor(self):
-    print(f'Descructor of {self} called')
-ecs.Entity.__del__ = destructor
+
+class Comps(Enum):
+    NAME = auto()
+    POSITION = auto()
+    VELOCITY = auto()
+    LIFETIME = auto()
+    PRINTABLE = auto()
+
 
 @dataclass
 class Position:
     x: float
     y: float
 
+
 @dataclass
 class Velocity:
     dx: float
     dy: float
 
-ten_seconds_walker = ecs.Entity(
-    ('name', '10 Seconds Walker'),
-    ('position', Position(0, 0)),
-    ('velocity', Velocity(0, 5)),
-    ('lifetime', 10),
-    ('printable', True),
-    tag='Walker',
-)
 
-def motion_system(e, position, velocity, delta_time):
+ten_seconds_walker = ecs.create_entity(tag='Walker')
+ecs.add_component(ten_seconds_walker, Comps.NAME, '10 Seconds Walker')
+ecs.add_component(ten_seconds_walker, Comps.POSITION, Position(0, 0))
+ecs.add_component(ten_seconds_walker, Comps.VELOCITY, Velocity(0, 5))
+ecs.add_component(ten_seconds_walker, Comps.LIFETIME, SimpleNamespace(time_left=10))
+ecs.add_component(ten_seconds_walker, Comps.PRINTABLE, True)
+
+
+def motion_system(dt, eid, position, velocity):
     # Note that assigning a new Vector to position would not make it back
     # into e.position.  But since we don't change the position instance
     # itself, we're safe.
-    position.x += velocity.dx * delta_time
-    position.y += velocity.dy * delta_time
+    position.x += velocity.dx * dt
+    position.y += velocity.dy * dt
 
-def lifetime_system(e, lifetime, delta_time):
-    # Note: lifetime is just an int and passed by value.  It cannot be
-    #       modified.  e.lifetime must be used here.
-    e.lifetime -= 1 * delta_time
-    if e.lifetime <= 0:
-        print(f'removing entity {e}')
-        e.exit()
 
-def render_system(e, printable):
+def lifetime_system(dt, eid, lifetime):
+    lifetime.time_left -= 1 * dt
+    if lifetime.time_left <= 0:
+        print(f'removing entity {eid}')
+        ecs.remove_entity(eid)
+
+
+def render_system(dt, eid, printable):
     # the printable component contains no data at all and can be ignored,
     # but it will still be passed as a parameter.
-    print(f'{e.name} has {e.lifetime} steps left, at {e.position}, walking towards {e.velocity}')
+    name, pos, velocity, lifetime = ecs.comps_of_eid(eid,
+                                                     Comps.NAME,
+                                                     Comps.POSITION,
+                                                     Comps.VELOCITY,
+                                                     Comps.LIFETIME)
+    print(f'{name} @ {pos.x, pos.y}, moving towards {velocity.dx, velocity.dy} '
+          f'for {lifetime.time_left} seconds')
 
-walker = ecs.entities_by_name('Walker')[0]
-print('My walker object is ', ecs.entities_by_name('Walker'))
-print('It has the following components');
-print(walker.components)
 
-ecs.run_system(render_system, ('printable'))
+print('*' * 72)
+print('1st run, explicit run_system')
+print('*' * 72)
+print(f'My walker object is {ten_seconds_walker}')
+print(f'It has the following components: {ecs.cids_of_eid(ten_seconds_walker)}')
+print(f'All entities: {ecs.eidx}')
+print(f'All components: {ecs.cidx}')
+print(f'All systems: {ecs.fidx}')
+
+ecs.run_system(1, render_system, Comps.PRINTABLE)
 for i in range(15):
-    ecs.run_system(motion_system, ('position', 'velocity'), 1)
-    ecs.run_system(lifetime_system, ('lifetime',), 1)
-    ecs.run_system(render_system, ('printable',))
+    ecs.run_system(1, motion_system, Comps.POSITION, Comps.VELOCITY)
+    ecs.run_system(1, lifetime_system, Comps.LIFETIME)
+    ecs.run_system(1, render_system, Comps.PRINTABLE)
 
+print(f'All entities: {ecs.eidx}')
+print(f'All components: {ecs.cidx}')
+print(f'All systems: {ecs.fidx}')
+
+print('*' * 72)
+print('2nd run, run_all_systems')
+print('*' * 72)
+ecs.add_system(motion_system, Comps.POSITION, Comps.VELOCITY)
+ecs.add_system(lifetime_system, Comps.LIFETIME)
+ecs.add_system(render_system, Comps.PRINTABLE)
+
+ten_seconds_walker = ecs.create_entity(tag='Walker')
+ecs.add_component(ten_seconds_walker, Comps.NAME, '10 Seconds Walker')
+ecs.add_component(ten_seconds_walker, Comps.POSITION, Position(0, 0))
+ecs.add_component(ten_seconds_walker, Comps.VELOCITY, Velocity(0, 5))
+ecs.add_component(ten_seconds_walker, Comps.LIFETIME, SimpleNamespace(time_left=10))
+ecs.add_component(ten_seconds_walker, Comps.PRINTABLE, True)
+
+print(f'My walker object is {ten_seconds_walker}')
+print(f'It has the following components: {ecs.cids_of_eid(ten_seconds_walker)}')
+print(f'All entities: {ecs.eidx}')
+print(f'All components: {ecs.cidx}')
+print(f'All systems: {ecs.fidx}')
+
+for i in range(15):
+    ecs.run_all_systems(1)
+
+print(f'All entities: {ecs.eidx}')
+print(f'All components: {ecs.cidx}')
+print(f'All systems: {ecs.fidx}')
--- tinyecs/tinyecs.py
+++ tinyecs/tinyecs.py
@@ -1,209 +1,97 @@
-"""The teeniest, tiniest ECS system
-
-In contrast to other systems, I only give you an Entity class.  No
-dedicated classes for components or systems/runners.
-
-An Entity instance is basically nothing more than a list of components.
-
-On creation, an entity is registered globally with each component it
-contains.  New components that are added later are registered automatically
-as well.
-
-Also, the component is added as an attribute to the Entity object. So after
-adding a 'name' component to a player, it can be accessed as player.name.
-
-Once entities are created and components have been added to them, you call
-the run_system method with a list of required components, the system
-function, and a list of optional parameters.
-
-Entities that are no longer needed can be removed from the registry by the
-remove() method.
-"""
+from uuid import uuid4
 
 eidx = {}
 cidx = {}
+fidx = {}
 
-class Entity:
-    """This is the only class this system has.  Each object entity is an
-    instance of this class.  The registry that maps entities to their
-    components is a module global a.k.a. a singleton.  Yepp, we could pack this
-    into its own ecs class and instantiate it, but fuck it!
-    """
 
-    def __init__(self, *components, tag=None):
-        """Entity([(name, component, ...], [tag=None]) -> Entity object
+def create_entity(tag=None, **kwargs):
+    global eidx, cidx
 
-            Create an Entity instance with optional components already added
-            (see add() below for details).
+    eid = tag if tag else str(uuid4())
+    eidx[eid] = {}
 
-            If you give the entity a tag (name), it can be queried by the
-            entities method.  Even if you don't give it a tag, you can query it
-            with 'None'.  Note, that multiple instances can have the same tag.
+    # Add optionally passed components
+    for cid, comp in kwargs.items():
+        add_component(eid, cid, comp)
 
-            Components are zero or more tuples containing the name and the
-            actual component data.
+    return eid
 
-                player = Entity(
-                    ('name': 'Ze mighty'),
-                    ('position', Vector(100, 42)),
-                    tag='player')
 
-                bullet = Entity(
-                    ('lifetime', 50),
-                    ('damage', 10),
-                    ('position', Vector(50, 50)))
-
-        """
-
-        global eidx, cidx
-
-        self.tag = tag
-
-        # Register the entity by tag
-        if tag not in eidx:
-            eidx[tag] = []
-        eidx[tag].append(self)
-
-        # Add optionally passed components
-        self.components = [self.add(name, comp) for name, comp in components]
-
-    def add(self, name, component):
-        """add(name, component) -> name
-
-            Add a named component to an entity
-
-                player.add('health', 100)
-
-        """
-
-        # Add component as full attribute addressable by name
-        setattr(self, name, component)
-
-        # add self to the list of entities that own this component
-        if name not in cidx:
-            cidx[name] = []
-        cidx[name].append(self)
-
-        return name
-
-    def remove(self, component):
-        """remove(component) -> None
-
-        Remove a component from an entity.  This removes the actual component
-        from the instance as well as from the component registry.
-        """
-
-        global cidx
-
-        # Delete it from the info list
-        self.components.remove(component)
-
-        # Delete the class attribute
-        del self.__dict__[component]
-
-        # Delete the entity from the component's registry
-        cidx[component].remove(self)
-
-    def exit(self):
-        """exit() -> None
-
-            Removes the Entity instance from the registry.  If the registry is
-            the only place that keeps a reference to the entity, the object
-            will then automatically be deleted.
-
-            Useful for objects with a lifetime that automatically delete
-            themselves
-
-                bullet.exit()
-
-        """
-        global eidx
-
-        # Delete this entity from all components in the components registry use
-        # 'list()' to create a copy, since remove() modifies self.components
-        # while we're looping over it
-        for c in list(self.components):
-            self.remove(c)
-
-        # Delete self from the Entity registry, also delete the slot for tag,
-        # if no entities with that name are left.
-        eidx[self.tag].remove(self)
-        if len(eidx[self.tag]) == 0:
-            del eidx[self.tag]
-
-    def component(self, component):
-        return getattr(self, component)
-
-def entities_by_name(tag):
-    """entities_by_name(tag) -> list of entities
-
-    Returns a list of entities that match the tag
-
-    Use this to access entities or their components outside systems.
-
-        player = tinyecs.entities_by_tag('player')[0]
-        bullets =tinyecs.entities_by_tag('projectiles')
-
-    """
-
+def remove_entity(eid):
+    # Ignore unknown eids, since we're removing anyways
     try:
-        return eidx[tag]
+        cids = eidx[eid].keys()
     except KeyError:
-        return []
+        return
 
-def run_system(fkt, conditions, *args, **kwargs):
-    """run_system(fkt, conditions, *args, **kwargs) -> None
+    for cid in cids:
+        del cidx[cid][eid]
 
-    Run fkt(entity, *components, *args, **kwargs) for all entities that contain all
-    components listed in conditions.
+    del eidx[eid]
 
-    Note that components can only be changed, if they are complex types.
-    An int() component will be passed by value and the change will never be
-    reflected in the entity object.  In that case use 'entity.component'
-    instead..  The semantics are the same as with normal function calls.
 
-        def blast_damage_system(e, comps, bomb):
-            health, position = comps
-            if distance(e.position, bomb.position) < bomb.blast_radius:
-                e.health -= bomb.damage
+def add_component(eid, cid, comp):
+    global eidx, cidx
 
-        player = Entity(('health', 100))
+    if eid not in eidx:
+        raise KeyError(f'{eid} not registered')
 
-        def health_system(e, comps):
-            # Change of health won't be transfered to e
-            health, = comps
-            if e.health <= 0:
-                e.exit()
+    if cid not in cidx:
+        cidx[cid] = {}
 
-        bomb = Bomb(position=(100, 100), damage=42)
-        Entity.run_system(blast_damage_system, ('health', 'position'), bomb)
-        Entity.run_system(health_system, ('health',))
+    cidx[cid][eid] = comp
+    eidx[eid][cid] = comp
 
-    """
 
-    entities = [ set(grep(_)) for _ in conditions ]
+def remove_component(eid, cid):
+    global eidx, cidx
 
-    # Now logical-and all entities for every condition (a.k.a. component)
-    # so we only get entities that contain all conditions.
-    matching = entities[0]
-    for e in entities[1:]:
-        matching &= e
-
-    for e in matching:
-        components = [ e.component(_) for _ in conditions ]
-        fkt(e, *components, *args, **kwargs)
-
-def grep(component):
-    """grep(name) -> list of entities
-
-        Return a list of entities that own this component.  Use this to
-        pass appropriate entities to the system/runner
-
-            sprites_that_can_render = Entity.grep('has-sprite')
-
-    """
-
+    # Ignore unknown cids or eids since we're removing anyways
+    # Also, no need to try each on their own
     try:
-        return cidx[component]
+        del cidx[cid][eid]
+        del eidx[eid][cid]
     except KeyError:
-        return []
+        pass
+
+
+def add_system(fkt, *comps):
+    fidx[fkt] = comps
+
+
+def remove_system(fkt):
+    # Ignore unregistered systems, since we're removing anyways
+    try:
+        del fidx[fkt]
+    except KeyError:
+        pass
+
+
+def eids_by_cids(*cids):
+    eid_sets = [set(cidx[cid].keys()) for cid in cids]
+
+    return list(set.intersection(*eid_sets))
+
+
+def cids_of_eid(eid):
+    return list(eidx[eid].keys())
+
+
+def comps_of_eid(eid, *cids):
+    if not cids:
+        return eidx[eid].values()
+
+    return [eidx[eid][cid] for cid in cids]
+
+
+def run_system(dt, fkt, *cids):
+    eids = eids_by_cids(*cids)
+
+    return {eid: fkt(dt, eid, *comps_of_eid(eid, *cids))
+            for eid in eids}
+
+
+def run_all_systems(dt):
+    return {fkt: run_system(dt, fkt, *comps)
+            for fkt, comps in fidx.items()}
