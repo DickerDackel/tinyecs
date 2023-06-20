8081117199abed99caf3ddb7dc6cf51d18293060
Michael Lamertz <michael.lamertz@gmail.com>
2023-06-20 14:39:07 +0200

    *bump*

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tinyecs',
-    version='0.1.8',
+    version='0.1.9',
     description='The teeniest, tiniest ECS system',
     author='Michael Lamertz',
     author_email='michael.lamertz@gmail.com',
