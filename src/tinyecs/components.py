f84ac05443b3c6f1a693f89d13959f295a9c59c2
Michael Lamertz <michael.lamertz@gmail.com>
2023-08-03 11:17:24 +0200

    Updated ESprite, added EVsprite
    ESprite now creates an empty default image and a rect placing it 1px
    outside the screen, so that every required attribute is available at
    instantiation time.
    
    EVsprite is a "virtual" sprite where `image` is a property provided by
    an `image_factory` passed to `init`.
    
    The interface to the `image_factory` also is its `image` attribute.

--- src/tinyecs/components.py
+++ src/tinyecs/components.py
@@ -13,8 +13,90 @@ from pygame import Vector2
 
 
 class ESprite(pygame.sprite.Sprite):
+    """A sprite class especially for ECS entities.
+
+    If an entity with a sprite component is removed, the sprite also needs to
+    be removed from all sprite groups.
+
+    `tinyecs` offers the `shutdown_` method for this.  If this is available,
+    ecs.remove_entity will call it when tearing down an entity.
+
+    Parameters
+    ----------
+    *groups : *pygame.sprite.Group()
+        Directly passed into parent class.  See `pygame.sprite.Sprite` for
+        details.
+
+    tag : hashable = None
+        A tag to identify this sprite.  Can e.g. be used for image caching.
+
+    """
+    def __init__(self, *groups, tag=None, image=None):
+        super().__init__(*groups)
+
+        if image is None:
+            self.image = pygame.surface.Surface((1, 1))
+        else:
+            self.image = image
+
+        self.rect = self.image.get_rect(bottomright=(-1, -1))
+
     def shutdown_(self):
         self.kill()
+
+
+class EVSprite(pygame.sprite.Sprite):
+    """A sprite class especially for ECS entities.
+
+    The E stands for the ECS, the V for virtual sprite.
+
+    If an entity with a sprite component is removed, the sprite also needs to
+    be removed from all sprite groups.
+
+    `tinyecs` offers the `shutdown_` method for this.  If this is available,
+    ecs.remove_entity will call it when tearing down an entity.
+
+    The "virtual" part of this entity is the image access.  The image is just
+    a property that calls a factory function instead.  That way, image
+    generation can be handed over to a different component.  The sprite itself
+    is only responsible to provide a link to the sprite group, which handles
+    the drawing.
+
+    Parameters
+    ----------
+    image_factory: callable
+        A zero parameter function that is expected to return a
+        `pygame.surface.Surface` object.
+
+    *groups: *pygame.sprite.Group()
+        Directly passed into parent class.  See `pygame.sprite.Sprite` for
+        details.
+
+    """
+    def __init__(self, image_factory, *groups):
+        super().__init__(*groups)
+        self.image_factory = image_factory
+
+        self._image = pygame.surface.Surface((1, 1))
+        self.rect = self._image.get_rect(bottomright=(-1, -1))
+
+    def shutdown_(self):
+        self.kill()
+
+    @property
+    def image(self):
+        new_image = self.image_factory.image
+        if new_image is self._image:
+            return self._image
+        else:
+            self._image = new_image
+            self.rect = self._image.get_rect(center=self.rect.center)
+
+        return self._image
+
+    @image.setter
+    def image(self, image):
+        raise RuntimeError('EVSprite.image is dynamically generated.')
 
 
 def dead_system(dt, eid, dead):
