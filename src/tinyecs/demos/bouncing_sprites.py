638f29ad3b83be170e22003af57c81e69419f305
Michael Lamertz <michael.lamertz@gmail.com>
2023-09-05 17:53:22 +0200

    FPS in titlebar and release boxes on mouseclick

--- src/tinyecs/demos/bouncing_sprites.py
+++ src/tinyecs/demos/bouncing_sprites.py
@@ -30,19 +30,27 @@ def bounding_box_system(dt, eid, world, position, momentum):
 
 
 def create_thing(group, world):
+    pos = pygame.mouse.get_pos()
+
     image = pygame.Surface((randint(5, 64), randint(5, 64)))
     image.fill(pygame.Color(randint(0, 255), randint(0, 255), randint(0, 255)))
 
     entity = ecs.create_entity()
     ecs.add_component(entity, 'sprite', Sprite(image, group))
-    ecs.add_component(entity, 'position', Vector2(world.center))
+    ecs.add_component(entity, 'position', Vector2(pos))
     v = Vector2(random() * 150 + 150, 0)
     v.rotate_ip(random() * 360)
     ecs.add_component(entity, 'momentum', v)
     ecs.add_component(entity, 'container', world)
 
 
+def create_a_bunch(group, world):
+    for i in range(16):
+        create_thing(group, world)
+
+
 def main():
+    TITLE = 'pygame minimal template'
     FPS = 60
     SCREEN = pygame.Rect(0, 0, 1024, 768)
 
@@ -57,9 +65,12 @@ def main():
     ecs.add_system(ecsc.sprite_system, 'sprite', 'position')
     ecs.add_system(bounding_box_system, 'container', 'position', 'momentum')
 
-    for i in range(16):
-        create_thing(group, SCREEN)
+    create_a_bunch(group, SCREEN)
 
+    click = pygame.Font(None, 48).render('Click!', True, 'white')
+    click_rect = click.get_rect(center=SCREEN.center)
+
+    emit = False
     running = True
     while running:
         dt = clock.get_time() / 1000.0
@@ -69,24 +80,35 @@ def main():
                 case pygame.QUIT:
                     running = False
 
-                case pygame.KEYDOWN:
-                    match e.key:
-                        case pygame.K_ESCAPE:
-                            running = False
-                        case pygame.K_SPACE:
-                            for i in range(16):
-                                create_thing(group, SCREEN)
+                case pygame.MOUSEBUTTONDOWN if e.button == 1:
+                    emit = True
+
+                case pygame.MOUSEBUTTONUP if e.button == 1:
+                    emit = False
+
+                case pygame.KEYDOWN if e.key == pygame.K_ESCAPE:
+                    running = False
+                case pygame.KEYDOWN if pygame.K_SPACE:
+                    create_a_bunch(group, SCREEN)
+
+        if emit:
+            create_a_bunch(group, SCREEN)
 
         screen.fill('black')
 
         group.update(dt)
         ecs.run_all_systems(dt)
 
+        screen.blit(click, click_rect)
         group.draw(screen)
 
         pygame.display.update()
         clock.tick(FPS)
-        pygame.display.set_caption(f'FPS: {clock.get_fps():5.2f}, {len(group)} sprites')
+        runtime = pygame.time.get_ticks()/1000
+        fps = clock.get_fps()
+        sprites = len(group)
+
+        pygame.display.set_caption(f'{TITLE} - {runtime=:.2f}  {fps=:.2f}  {sprites=}')
 
     pygame.quit()
 
