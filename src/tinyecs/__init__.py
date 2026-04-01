2ff1c6e51f7ec9c800519b5a69f7e0dbf88650fd
Michael Lamertz <michael.lamertz@gmail.com>
2026-04-01 17:07:40 +0200

    Comment typo

--- src/tinyecs/__init__.py
+++ src/tinyecs/__init__.py
@@ -263,7 +263,7 @@ def add_component(eid, cid, comp):
     if cid not in cidx:
         cidx[cid] = {}
 
-    # Make sure, when replacing a component, the old one is remove from oidx
+    # Make sure, when replacing a component, the old one is removed from oidx
     try:
         old_comp = eidx[eid][cid]
         del oidx[id(old_comp)]
