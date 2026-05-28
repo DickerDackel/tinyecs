ee1a04ea7ed1f485037726d859b935fb1b08974c
Michael Lamertz <michael.lamertz@gmail.com>
2026-05-28 15:51:47 +0200

    Still rtd...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('../../src'))
+# import os
+# import sys
+#
+# sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'tinyecs'
 copyright = '2026, Michael "Dicker Dackel" Lamertz'
